b9832575dd549857cf504c3e7b109541d75669bd
Juliane Ritter <ritter.jul@gmail.com>
2020-04-22 12:12:41 +0200

    run database migration locally

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,9 +1,11 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_path = os.environ['DATABASE_URL']
+
+#database_path = "postgres://juliane:POSTGRES@localhost:5432/heroku_sample"
 
 db = SQLAlchemy()
 
